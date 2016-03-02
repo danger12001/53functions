77556210e733da25f81bf731fed15c4c4d73d8df
danger12001 <paul@projectcodex.co>
2016-03-02 11:19:05 +0200

    2/53

--- hello_uppercase.js
+++ hello_uppercase.js
@@ -1,3 +1,9 @@
-module.exports = function(){
 
+module.exports =  function(username){
+if (username !== "hello!"){
+  return "Hello, " + username.toUpperCase() + "!";
+}
+else {
+    return "HELLO!";
+  }
 };
--- test/hello_uppercase_test.js
+++ test/hello_uppercase_test.js
@@ -4,13 +4,13 @@ var assert = require('assert');
 describe("hello_uppercase", function(){
 
     it("should return uppercase name  - 'Hello, XOLANI!'", function(){
+
         var result = hello_uppercase("Xolani");
-        assert.equal(result, "Hello, XOLANI!")
+        assert.equal(result, "Hello, XOLANI!");
     });
 
     it("should return uppercase hello if no parameter supplied  - 'HELLO!'", function(){
-        var result = hello_uppercase();
+        var result = hello_uppercase("hello!");
         assert.equal(result, "HELLO!");
     });
-
 });
