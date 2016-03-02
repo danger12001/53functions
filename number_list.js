8c9d3c86194d370741c945358e04261d7c6eaf61
danger12001 <paul@projectcodex.co>
2016-03-02 11:46:59 +0200

    4/53

--- number_list.js
+++ number_list.js
@@ -1,3 +1,7 @@
-module.exports = function(){
-
+module.exports = function(n){
+var Num = [];
+  for (x = 1; x <= n; x++){
+    Num.push(x);
+  }
+  return Num;
 };
--- test/number_list_test.js
+++ test/number_list_test.js
@@ -5,7 +5,7 @@ describe("number_list", function(){
 
     it("should return [1, 2, 3, 4, 5] when number_list called with 5", function(){
         var result = number_list(5);
-        assert.deepEqual(result, [1,2,3,4,5])
+        assert.deepEqual(result, [1,2,3,4,5]);
     });
 
 });
