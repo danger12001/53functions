7c54f2cc0d7fddac3a3b7e2f18391743bd10e93d
danger12001 <paul@projectcodex.co>
2016-03-02 12:03:24 +0200

    5/53

--- sum_numbers.js
+++ sum_numbers.js
@@ -1,3 +1,8 @@
-module.exports = function(){
-    
-}
+module.exports = function(n) {
+  var Num = 0;
+  for (x = 1; x <= n; x++) {
+    Num = Num + x;
+  }
+  return Num;
+
+};
--- test/sum_numbers_test.js
+++ test/sum_numbers_test.js
@@ -5,12 +5,11 @@ describe("sum_numbers", function(){
 
     it("should return 15 when called with 5", function(){
         var result = sum_numbers(5);
-        assert.equal(result, 15)
+        assert.equal(result, 15);
     });
 
     it("should return 0 when called with 0", function(){
         var result = sum_numbers(0);
         assert.equal(result, 0);
     });
-
 });
