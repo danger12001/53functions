9df1815785e9df48507e77d03d31702b2f577aa8
danger12001 <paul@projectcodex.co>
2016-04-01 13:11:24 +0200

    20/53

---
+++ occurs_most.js
@@ -0,0 +1,20 @@
+module.exports = function(str){
+  var letters = str.replace(/ /g,"").split("");
+  console.log(letters);
+  if(letters.length == 0)
+    return null;
+    var modeMap = {};
+    var maxEl = letters[0], maxCount = 1;
+    for(var x = 0; x < letters.length; x++){
+        var el = letters[x];
+          if(modeMap[el] == null)
+            modeMap[el] = 1;
+          else
+            modeMap[el]++;
+              if(modeMap[el] > maxCount){
+                  maxEl = el;
+                  maxCount = modeMap[el];
+                                        }
+                                                }
+  return maxEl;
+}
