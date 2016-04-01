ec0f37fd8cc1d74bfc373347ddf08a56c9de8bb6
danger12001 <paul@projectcodex.co>
2016-04-01 12:50:27 +0200

    19/53

--- ends.js
+++ ends.js
@@ -5,5 +5,20 @@ var last_letters = [];
 for(x=0;x<words.length;x++){
   last_letters.push(words[x].charAt(words[x].length - 1));
 }
-console.log(last_letters);
-};
+if(last_letters.length == 0)
+  return null;
+  var modeMap = {};
+  var maxEl = last_letters[0], maxCount = 1;
+  for(var x = 0; x < last_letters.length; x++){
+      var el = last_letters[x];
+        if(modeMap[el] == null)
+          modeMap[el] = 1;
+        else
+          modeMap[el]++;
+            if(modeMap[el] > maxCount){
+                maxEl = el;
+                maxCount = modeMap[el];
+                                      }
+                                              }
+return maxEl;
+}
