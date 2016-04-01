efd65043a641bbd775d89145a4e032bd0b99e985
danger12001 <paul@projectcodex.co>
2016-04-01 12:45:34 +0200

    last letters in an array

--- ends.js
+++ ends.js
@@ -1,3 +1,9 @@
-module.exports = function(){
-
+module.exports = function(str){
+var words = str.split(" ");
+console.log(words);
+var last_letters = [];
+for(x=0;x<words.length;x++){
+  last_letters.push(words[x].charAt(words[x].length - 1));
+}
+console.log(last_letters);
 };
